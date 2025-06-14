4424c35c4bcb7993177591645d716be15385169c
Tariq Atlas <tariqatlas7@gmail.com>
2025-06-14 11:18:23 +0430

    add the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function HomePage() {
-  return <div>hi</div>;
+import { redirect } from "next/navigation";
+
+export default function RedirectPage() {
+  // TODO: Replace this with actual session retrieval logic
+  const session = {
+    userId: "123",
+  };
+
+  if (session.userId == null) {
+    redirect("/auth/login");
+  } else {
+    redirect("/event-types");
+  }
 }
